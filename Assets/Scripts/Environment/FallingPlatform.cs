17ec306ee413e42fe7f14f2a60e903ed1de2d55a
alegottu <33403572+alegottu@users.noreply.github.com>
2021-04-02 15:40:31 -0700

    Falling platform fix and animation fixes

--- Assets/Scripts/Environment/FallingPlatform.cs
+++ Assets/Scripts/Environment/FallingPlatform.cs
@@ -3,20 +3,17 @@ using UnityEngine;
 public class FallingPlatform : Platform
 {
     [SerializeField] private Animator anim = null;
-    [SerializeField] private Collider2D collider2d = null;
     [SerializeField] private float timeToFall = 1;
     [SerializeField] private float restoreCooldown = 1;
 
     private bool fall = false;
     private float fallTimer = 0;
     private float restoreTimer = 0;
-    private Vector3 initial = Vector3.zero;
 
     private void Awake()
     {
         fallTimer = timeToFall;
         restoreTimer = restoreCooldown;
-        initial = transform.position;
     }
 
     protected override void OnPlayerEnter()
@@ -29,15 +26,9 @@ public class FallingPlatform : Platform
     {
         if (restoreTimer <= 0)
         {
-            transform.position = initial;
-            restoreTimer = 0;
-            collider2d.enabled = true;
+            anim.SetTrigger("Restore");
+            restoreTimer = restoreCooldown;
             fall = false;
-        }
-
-        if (fallTimer <= 0)
-        {
-            collider2d.enabled = false;
         }
 
         if (fall)
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -3,7 +3,6 @@
 public class PlayerInput : MonoBehaviour
 {
     [HideInInspector] public float movement = 0;
-    [HideInInspector] public Vector3 mousePos = Vector3.zero;
     [HideInInspector] public bool attack = false;
     [HideInInspector] public bool jump = false;
     [HideInInspector] public bool jumpEnter = false;
@@ -11,12 +10,10 @@ public class PlayerInput : MonoBehaviour
     [HideInInspector] public bool grab = false;
 
     [SerializeField] private float smoothRate = 1;
-    [SerializeField] private Camera cam = null;
 
     private void Update()
     {
         movement = Mathf.MoveTowards(movement, InputManager.Instance.GetAxisRaw("Horizontal"), smoothRate);
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         attack = InputManager.Instance.GetButtonDown("Attack");
         jump = InputManager.Instance.GetButton("Jump");
         jumpEnter = InputManager.Instance.GetButtonDown("Jump");
