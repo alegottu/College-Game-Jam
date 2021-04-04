a1b8c5cb793ff2e5a294ca9ef318e0b731239a5b
alegottu <33403572+alegottu@users.noreply.github.com>
2021-04-04 14:50:52 -0700

    Added stage select

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelSelect : SceneTransition
+{
+    private static Dictionary<int, string> levels = new Dictionary<int, string>
+    {
+        [0] = "Tutorial",
+        [1] = "StageOne",
+        [2] = "StageTwo",
+        [3] = "StageThree"
+    };
+
+    public void OnClick(int level)
+    {
+        SceneController.Instance.LoadLevel(levels[level]);
+    }
+}
